c9840ff2426c377b8ef132d0e36e8e60d2c2dc93
Arun <guideme360@gmail.com>
2018-07-09 23:29:03 +0530

    Spring JDBC Template

---
+++ SpringCrud/src/main/java/com/guideme/crud/dto/Employee.java
@@ -0,0 +1,48 @@
+package com.guideme.crud.dto;
+
+public class Employee {
+	private String firstname, lastname, email;
+	private int employeeid;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String firstname) {
+		super();
+		this.firstname = firstname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getEmployeeid() {
+		return employeeid;
+	}
+
+	public void setEmployeeid(int employeeid) {
+		this.employeeid = employeeid;
+	}
+
+}
